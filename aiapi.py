5d8e353cc3767e642dc428952196e896036e6437
tigerhu <huxy99@yahoo.com>
2023-03-08 23:21:58 -0600

    Add Ajax functions to send question to server and get answer from chatGPT

--- aiapi.py
+++ aiapi.py
@@ -4,16 +4,16 @@ import config
 openai.api_key = config.DevelopmentConfig.OPENAI_KEY
 
 
-def getChatResponse(message):
+def getChatResponse(prompt):
     """Return the response to a chat message."""
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
     ]
-    messages.append({"role": "user", "content": message})
+    messages.append({"role": "user", "content": prompt})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=messages)
     try:
-        answer = response["choices"][0]["message"]["content"]
+        answer = response["choices"][0]["message"]["content"].replace("\n", "<br>")
     except IndexError:
         answer = "Oops, I didn't get that. Please try again."
     return answer
--- app.py
+++ app.py
@@ -16,6 +16,10 @@ app.register_error_handler(404, page_not_found)
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
+    """Return the homepage and render questions and answers."""
+    if request.method == 'POST':
+        question = request.form.get('prompt')
+        return jsonify({'answer': aiapi.getChatResponse(question)}), 200
     return render_template('index.html', **locals())
 
 
